d21e68be482c4f7a83705d00f13c6a645472c297
shofwahknk <shofwahknk@gmail.com>
2024-03-15 13:47:45 +0700

    PRACTICUM QUESTIONS

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProductController extends Controller{
+    public function foodBeverage(){
+        return view('food_beverage');
+    }
+
+    public function beautyHealth(){
+        return view('beauty_health');
+    }
+
+    public function homeCare(){
+        return view('home_care');
+    }
+
+    public function babykid(){
+        return view('baby_kid');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Home Page 
+Route::get('/', [HomeController::class,'index']);
+
+// Product Routes 
+Route::get('/category/food-beverage',[ProductController::class, 'foodBeverage']);
+Route::get('/category/beauty-health',[ProductController::class, 'beautyHealth']);
+Route::get('/category/home-care',[ProductController::class, 'homeCare']);
+Route::get('/category/baby-kid',[ProductController::class, 'babyKid']);
+
+// User Page 
+Route::get('user/{id}/name/{name}', [UserController::class, 'show']);
+
+// Sales Page
+Route::get('/sales',[SalesController::class, 'index']);
